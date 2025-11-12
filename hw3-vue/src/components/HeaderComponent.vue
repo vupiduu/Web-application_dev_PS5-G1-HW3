0ab3d690447111016de2d3934050da357a547252
Karl-Ustav Kõlar <karl-ustav.kolar@askend.com>
2025-11-12 12:11:38 +0200

    Add header and footer component

---
+++ hw3-vue/src/components/HeaderComponent.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="app-header">
+    <h1>{{ title }}</h1>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Signup</a>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.app-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100%;
+  background-color: #42b983;
+  padding: 1rem;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+nav a {
+  color: white;
+  margin-left: 1rem;
+  text-decoration: none;
+  padding: 1rem;
+  background-color: #42a36b;
+  border-radius: 2rem;
+  transition: background-color 200ms ease, color 200ms ease;
+  will-change: background-color, color;
+}
+
+nav a:hover {
+  color: #35495e;
+  background-color: #54c77f;
+
+}
+</style>
